bc20f701b9c33f9fd41ac2eb6b63ad98df69a127
pamelarocha <gabrielpr.martins@gmail.com>
2022-06-22 19:07:29 -0300

    first commit

---
+++ endpoints/user_post.php
@@ -0,0 +1,27 @@
+<?php 
+
+function api_user_post($request) {
+  $email = $request['email'];
+  $name = $request['name'];
+  $phone = $request['phone'];
+  $password = $request['password'];
+
+  
+
+  $response = [
+    'ID' => '2',
+    'user_login' => 'meu_usuario'
+  ];
+
+  return rest_ensure_response($response);
+}
+
+function register_api_user_post() {
+  register_rest_route('api', '/user', [
+    'methods' => WP_REST_Server::CREATABLE,
+    'callback' => 'api_user_post',
+  ]);
+}
+add_action('rest_api_init', 'register_api_user_post');
+
+?>
