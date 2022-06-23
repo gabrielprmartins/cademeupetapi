2b3c95306fe1bd03acb317e372efd13687e9aab0
pamelarocha <gabrielpr.martins@gmail.com>
2022-06-23 20:36:02 -0300

    Password Lost

--- functions.php
+++ functions.php
@@ -17,6 +17,8 @@ require_once $dirbase . '/endpoints/pet_post.php';
 require_once $dirbase . '/endpoints/pet_delete.php';
 require_once $dirbase . '/endpoints/pet_get.php';
 
+require_once $dirbase . '/endpoints/password.php';
+
 update_option('large_size_w', 1000);
 update_option('large_size_h', 1000);
 update_option('large_crop', 1);
